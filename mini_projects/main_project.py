72492ee2de27382b1ca641ffb63514b7098dd1e1
Muhammad_Haris <hariscool987@gmail.com>
2025-12-09 04:21:37 -0800

    Chai_Time_Snacks_Python_Project
    # Chai Time Snacks Python Project
    
    A collection of beginner-friendly Python scripts that demonstrate the use of conditional statements, user input, and decision-making in Python. 
    
    ## Projects Included:
    1. **Kettle Boiled Check** - Checks if the kettle is ready for making tea.
    2. **Snack Selection** - Lets users choose a snack and responds based on availability.
    3. **Cup Size Price** - Shows the price of a cup based on selected size.
    4. **Device Temperature Monitoring** - Monitors device status and temperature.
    
    ## How to Run:
    1. Clone the repository.
    2. Open the desired `.py` file.
    3. Run it in Python 3.x.
    4. Follow the on-screen instructions.
    
    ## Skills Learned:
    - Conditional statements (`if`, `elif`, `else`)
    - Boolean logic
    - Nested conditions
    - User input handling
    - String manipulation

---
+++ mini_projects/main_project.py
@@ -0,0 +1,21 @@
+print("====WELL COME TO OVER LIBARY====")
+print("1. View all Books")
+print("2. Add Book")
+print("3. Delete Book")
+print("4. Exit")
+
+
+choice = input("Enter your choice (1-4): ")
+
+if choice ==2:
+    print("For adding new Book give me some info of book")
+    book_name=input("Enter the name of book")
+    Author=input("Enter the Author name")
+
+
+elif choice==2:
+    print("Which book you want to delete:")
+    input("Enter the name of book you want to delete")
+   
+    
+
